f89c0cacacc624059028aebc144db32db149810f
MohammedAlzayyan <mohammedznn1999@gmail.com>
2023-03-21 10:11:02 +0300

    first commit

---
+++ src/lib/@heroicons/index.ts
@@ -0,0 +1,27 @@
+import {
+  ExclamationTriangleIcon as ErrorIconMini,
+  CheckCircleIcon as CheckCircleIconMini,
+  EyeIcon as EyeIconMini,
+  EyeSlashIcon as EyeSlashIconMini,
+  ChevronLeftIcon as ChevronLeftIconMini,
+  ArrowUpTrayIcon as ArrowUpTrayIconMini,
+  XMarkIcon as XMarkIconMini,
+  InformationCircleIcon as InformationCircleIconMini,
+  CheckIcon as CheckIconMini,
+  ChevronDownIcon as ChevronDownIconMini,
+} from "@heroicons/react/20/solid";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+
+export {
+  ErrorIconMini,
+  CheckCircleIconMini,
+  EyeIconMini,
+  EyeSlashIconMini,
+  ChevronLeftIconMini,
+  ArrowUpTrayIconMini,
+  XMarkIconMini,
+  InformationCircleIconMini,
+  InformationCircleIcon,
+  CheckIconMini,
+  ChevronDownIconMini,
+};
